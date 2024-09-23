4cddd8ba133e3a168e931479a8b1ed040a72c157
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-09-23 22:29:33 +0900

    chore: remove garbages

--- push.bat
+++
@@ -1,5 +0,0 @@
-set version=0.7
-docker build -t guitarrapc/agonessample-BackendServer:%version% -f src/BackendServer/Dockerfile .
-docker build -t guitarrapc/agonessample-FrontEnd:%version% -f src/FrontEnd/Dockerfile .
-docker push guitarrapc/agonessample-BackendServer:%version%
-docker push guitarrapc/agonessample-FrontEnd:%version%
