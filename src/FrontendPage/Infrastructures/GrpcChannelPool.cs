05a2b98ccc8522bc889fcf49bcfd49e179f2c5cb
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-03-24 20:16:43 +0900

    Update GrpcChannelPool.cs

--- src/FrontendPage/Infrastructures/GrpcChannelPool.cs
+++ src/FrontendPage/Infrastructures/GrpcChannelPool.cs
@@ -24,7 +24,7 @@ public class GrpcChannelPool : IDisposable
                 return CreateChannel(host);
             default:
                 return channel;
-        };
+        }
     }
 
     public void Dispose()
